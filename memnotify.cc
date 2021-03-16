33190e028c5d43134bd294e20e56c9a9f59c0e45
Javier Flores Assad <hologramas@hotmail.com>
2021-03-16 10:42:10 -0700

    relax loop, improve printing

--- memnotify.cc
+++ memnotify.cc
@@ -1,52 +1,81 @@
 #include <windows.h>
 #include <stdio.h>
 
+enum class MemoryState : int {
+  None,
+  Low,
+  High,
+};
+
+constexpr char* state_names[] = {
+  "Medium available",
+  "Low available",
+  "High available",
+};
+
+constexpr DWORD still_print_cadence = (30 * 1000);
+
+DWORD GetAvailablePhysicalMemMB() {
+  MEMORYSTATUSEX mem_info;
+  mem_info.dwLength = sizeof(mem_info);
+  ::GlobalMemoryStatusEx(&mem_info);
+  return mem_info.ullAvailPhys / (1024 * 1024);
+}
+
 int main() {
-  HANDLE low = ::CreateMemoryResourceNotification(::LowMemoryResourceNotification);
-  if (low == NULL) {
+  HANDLE low_event = ::CreateMemoryResourceNotification(::LowMemoryResourceNotification);
+  if (!low_event) {
     printf("CreateMemoryResourceNotification failed for low. GLE %u\n", ::GetLastError());
-    return -1;
+    return ::GetLastError();
   }
 
-  HANDLE high = ::CreateMemoryResourceNotification(::HighMemoryResourceNotification);
-  if (high == NULL) {
+  HANDLE high_event = ::CreateMemoryResourceNotification(::HighMemoryResourceNotification);
+  if (!high_event) {
+    ::CloseHandle(low_event);
     printf("CreateMemoryResourceNotification failed for high. GLE %u\n", ::GetLastError());
-    return -1;
+    return ::GetLastError();
   }
 
-  puts("Waiting for memory notifications\n");
+  puts("Waiting for memory notifications. Press CTRL+C to exit.\n");
 
-  bool low_last = false;
-  bool high_last = false;
-  HANDLE handles[2] = {low, high};
+  int exit_code = 0;
+  HANDLE handles[2] = {low_event, high_event};
+  MemoryState previous_state = MemoryState::None;
+  DWORD last_still = ::GetTickCount();
   for (;;) {
-    const DWORD wait_result = WaitForMultipleObjects(2, handles, FALSE, INFINITE);
-    switch (wait_result) {
-      case WAIT_OBJECT_0:
-        if (!low_last) {
-          printf("Low mem is signalled. Tick %u\n", ::GetTickCount());
-          low_last = true;
-        }
-        high_last = false;
-        break;
+    MemoryState new_state;
+    const DWORD result = ::WaitForMultipleObjects(2, handles, FALSE, 0);
+    if (result == WAIT_OBJECT_0) {
+      new_state = MemoryState::Low;
+    } else if (result == WAIT_OBJECT_0 + 1) {
+      new_state = MemoryState::High;
+    } else if (result == WAIT_TIMEOUT) {
+      new_state = MemoryState::None;
+    } else {
+      exit_code = ::GetLastError();
+      printf("Unexpected wait result: %u. GLE: %u\n", result, exit_code);
+      break;
+    }
 
-      case WAIT_OBJECT_0 + 1:
-        if (!high_last) {
-          printf("High mem is signalled. Tick %u\n", ::GetTickCount());
-          high_last = true;
-        }
-        low_last = false;
-        break;
-      
-      default:
-        printf("Unexpected wait result: %u. GLE: %u\n", wait_result, ::GetLastError());
-        high_last = false;
-        low_last = false;
-        break;
+    const DWORD now_tick = ::GetTickCount();
+    if (new_state != previous_state) {
+      printf("[%u] Moved from %s to %s memory state. Free MB: %u\n", now_tick,
+             state_names[(int)previous_state],
+             state_names[(int)new_state],
+             GetAvailablePhysicalMemMB());
+      previous_state = new_state;
+    } else if (now_tick - last_still > still_print_cadence) {
+      printf("[%u] Still in %s memory state. Free MB: %u\n", now_tick,
+             state_names[(int)new_state],
+             GetAvailablePhysicalMemMB());
+      last_still = now_tick;
+    } else {
+      // Give the CPU core some room.
+        Sleep(64);
     }
   }
 
-  ::CloseHandle(high);
-  ::CloseHandle(low);
-  return 0;
+  ::CloseHandle(high_event);
+  ::CloseHandle(low_event);
+  return exit_code;
 }
