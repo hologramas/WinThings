d61b9aa66e4e19d48a41388be9540c0768812399
Javier Flores Assad <hologramas@hotmail.com>
2022-02-22 09:07:44 -0800

    Adding quote variants to cmdhash

--- cmdhash.cc
+++ cmdhash.cc
@@ -33,14 +33,49 @@ static std::wstring NormalizeCommandLine(const std::wstring& original_commandlin
 
 constexpr wchar_t c_edge_stable[] = L"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
 
-int wmain(int argc, wchar_t* argv[]) {
-  std::wstring cmdline(c_edge_stable);
-  cmdline = cmdline.append(L" ");
-  cmdline = cmdline.append(argv[1]);
-  std::wstring commandline = NormalizeCommandLine(cmdline);
+static
+void PrintCommandLineHash(const std::wstring& program, const std::wstring& args, bool quote_program, bool quote_args) {
+
+  std::wstring raw_cmdline(program);
+  if (quote_program) {
+    raw_cmdline.insert(0, L"\"");
+    raw_cmdline.append(L"\"");
+  }
+
+  if (!args.empty()) {
+    if (quote_args) {
+      raw_cmdline.append(L" ");
+      raw_cmdline.append(L"\"");
+      raw_cmdline.append(args);
+      raw_cmdline.append(L"\"");
+    } else {
+      raw_cmdline.append(L" ");
+      raw_cmdline.append(args);
+    }
+  }
+
   std::hash<std::wstring> StringHash;
-  auto hash = static_cast<uint32_t>(StringHash(commandline));
-  fprintf(stdout, "Command line: %ws\n", commandline.c_str());
+  std::wstring command_line(NormalizeCommandLine(raw_cmdline));
+  const uint32_t hash = (uint32_t)StringHash(command_line);
+  fprintf(stdout, "Command line: %ws\n", command_line.c_str());
   fprintf(stdout, "Hash: %u\n", hash);
+}
+
+int wmain(int argc, wchar_t* argv[]) {
+  std::wstring args;
+  if (argc > 1) {
+    int i = 1;
+    while (i < argc -1) {
+      args.append(argv[i]);
+      args.append(L" ");
+      ++i;
+    }
+    args.append(argv[i]);
+  }
+
+  PrintCommandLineHash(c_edge_stable, args, false /* quote_program */, false /* quote_args */);
+  PrintCommandLineHash(c_edge_stable, args, false /* quote_program */, true /* quote_args */);
+  PrintCommandLineHash(c_edge_stable, args, true /* quote_program */, false /* quote_args */);
+  PrintCommandLineHash(c_edge_stable, args, true /* quote_program */, true /* quote_args */);
   return 0;
 }
